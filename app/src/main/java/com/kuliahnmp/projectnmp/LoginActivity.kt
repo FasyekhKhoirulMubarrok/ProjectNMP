41f6248ed68719610e28e6601debb2de0f888264
FasyekhKhoirulMubarrok <s160418037@student.ubaya.ac.id>
2020-11-02 12:49:22 +0700

    SignUp dan Login Code

--- app/src/main/java/com/kuliahnmp/projectnmp/LoginActivity.kt
+++ app/src/main/java/com/kuliahnmp/projectnmp/LoginActivity.kt
@@ -1,11 +1,48 @@
 package com.kuliahnmp.projectnmp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val q = Volley.newRequestQueue(this)
+            val url = "http://ubaya.prototipe.net/nmp160418005/loginproses.php"
+            val stringRequest = object : StringRequest(
+                Request.Method.POST, url,
+                Response.Listener {
+                    Log.d("cekparams", it)
+                    val intent = Intent(this, MainActivity::class.java);
+                    startActivity(intent)
+                    finish();
+                },
+                Response.ErrorListener {
+                    Log.d("cekparams", it.message.toString())
+                }
+            )
+            {
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["username"] = txtUsername.text.toString();
+                    params["password"] = txtPassword.text.toString();
+                    return params
+                }
+            }
+            q.add(stringRequest)
+        }
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java);
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/kuliahnmp/projectnmp/SignupActivity.kt
+++ app/src/main/java/com/kuliahnmp/projectnmp/SignupActivity.kt
@@ -1,11 +1,46 @@
 package com.kuliahnmp.projectnmp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        btnSignupOnSG.setOnClickListener {
+            if(txtPasswordSG.text.toString() == txtConfirmPassSG.text.toString()) {
+                val q = Volley.newRequestQueue(this)
+                val url = "http://ubaya.prototipe.net/nmp160418005/signupproses.php"
+                val stringRequest = object : StringRequest(
+                    Request.Method.POST, url,
+                    Response.Listener {
+                        Log.d("cekparams", it)
+                        val intent = Intent(this, LoginActivity::class.java);
+                        startActivity(intent)
+                        finish();
+                    },
+                    Response.ErrorListener {
+                        Log.d("cekparams", it.message.toString())
+                    }
+                ) {
+                    override fun getParams(): MutableMap<String, String> {
+                        val params = HashMap<String, String>()
+                        params["email"] = txtEmail.text.toString();
+                        params["username"] = txtUsernameSG.text.toString();
+                        params["password"] = txtPasswordSG.text.toString();
+                        return params
+                    }
+                }
+                q.add(stringRequest)
+            }
+        }
     }
 }
