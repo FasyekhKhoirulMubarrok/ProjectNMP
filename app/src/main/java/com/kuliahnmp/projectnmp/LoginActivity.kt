719da5106f51d3d9c636223313481716cf0c1608
FasyekhKhoirulMubarrok <s160418037@student.ubaya.ac.id>
2020-11-11 18:43:42 +0700

    Data User masuk ke class

--- app/src/main/java/com/kuliahnmp/projectnmp/LoginActivity.kt
+++ app/src/main/java/com/kuliahnmp/projectnmp/LoginActivity.kt
@@ -50,7 +50,7 @@ class LoginActivity : AppCompatActivity() {
             {
                 override fun getParams(): MutableMap<String, String> {
                     val params = HashMap<String, String>()
-                    params["username"] = txtUsername.text.toString();
+                    params["username"] = txtEmail.text.toString();
                     params["password"] = txtPassword.text.toString();
                     return params
                 }
