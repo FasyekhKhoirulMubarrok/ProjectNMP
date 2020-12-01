698c46c6223fe956a63db76333d60d2f9118713a
andrejustinnn <62698336+andrejustinnn@users.noreply.github.com>
2020-12-01 10:41:15 +0800

    HISTORY SUDAH TAMPIL TAPI MASIH BUG, 1 order id tidak masuk dalam 1 card. harusnya 1 order id jadi 1 card. foto sudah keluar tp lemot. lemot kemungkinan grgr 2 volley di baca data di HistoryFragment line 120.
    sm kadang2 keluar karena outbound index di ProductHistoAdapter.kt di line 46 sm 19

--- app/src/main/java/com/kuliahnmp/projectnmp/History.kt
+++ app/src/main/java/com/kuliahnmp/projectnmp/History.kt
@@ -1,10 +1,9 @@
 package com.kuliahnmp.projectnmp
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 data class History(val id:Int,
                    val orderid: Int,
                    val user_id: Int,
                 val orderdate:String,
-                val jumitem:Int, var productId: Int,var qty: Int,var subtotal:Int)
+                val jumitem:Int)
--- app/src/main/java/com/kuliahnmp/projectnmp/productHistory.kt
+++
@@ -1,7 +0,0 @@
-package com.kuliahnmp.projectnmp
-
-class productHistory(val id:Int, val judul:String,
-                     val deskripsi:String,
-                     val image_url:String,
-                     val harga:Int) {
-}
